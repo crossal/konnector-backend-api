eaf61fc9e0182537174ff5d53ed2ae9cb4c10658
crossal <crossal@users.noreply.github.com>
2023-02-09 21:41:13 +0000

    Setup docker image to have tls cert (#11)

---
+++ docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "hello"
+ENVIRONMENT="$1"
+KEYSTORE_PASSWORD="$2"
+echo "hello2"
+echo "env $ENVIRONMENT"
+echo "pass $KEYSTORE_PASSWORD"
+
+if [ "$#" -ne 2 ]; then
+  echo "Illegal number of parameters (needed 2)"
+  exit 1
+fi
+
+if [ "$ENVIRONMENT" = "prod" ]
+then
+  certbot certonly --standalone -d konnector.io -d www.konnector.io --non-interactive --email root.konnector@gmail.com --agree-tos
+  cd /etc/letsencrypt/live/konnector.io
+  openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out keystore.p12 -name konnectorCerts -CAfile chain.pem -caname otherCerts -password "$2"
+fi
+
+java -jar /app.jar --network="host" --spring.profiles.active="$ENVIRONMENT"
